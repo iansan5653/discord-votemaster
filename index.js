552dd80b4b79870db50bb4430fbb65b1fbb6f957
Ian Sanders <iansan5653@gmail.com>
2017-10-07 23:43:29 -0400

    Initial basic testing setup.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code goes here
+const private = require('./private.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  } else if (message.content === 'Bye Votemaster!') {
+  	// Useful for quitting when Nodemon is watching for changes
+  	process.exit();
+  }
+});
+
+client.login(private.token);
---
+++ private_example.js
@@ -0,0 +1,3 @@
+// Get a token from https://discordapp.com/developers/applications/me
+// Replace YOUR TOKEN HERE with it and save as "private.js"
+module.exports.token = "YOUR TOKEN HERE";
