ba677e157f84a580875a516a595926c6d3a4b36a
Ian Sanders <iansan5653@gmail.com>
2017-10-09 02:24:57 -0400

    Created constructor for Poll objects, used to house poll data and votes.

--- index.js
+++ index.js
@@ -5,6 +5,71 @@ const client = new Discord.Client();
 const defaults = {
 	timeout: 30
 };
+var pollIndex = 0;
+
+const emoji = {
+	numbers: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'],
+	letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+		.map(value => 'regional_indicator_' + value),
+	yn: ['yes','no'],
+	maybe: 'shrug'
+};
+
+function Poll(opt) {
+	var args = opt.arguments;
+	this.name = opt.name;
+	this.id = pollIndex;
+		pollIndex++;
+
+	this.choices = {};
+	opt.choices.forEach((value, index) => {
+		this.choices[emoji[opt.emojiType][index]] = {
+			name: value,
+			votes: 0
+		};
+	});
+	if(args.maybe || args.idk) {
+		this.choices[emoji.maybe] = {
+			name: 'I don\'t know.',
+			votes: 0
+		};
+	}
+
+	this.lock = args.lock || false;
+	this.votedUsers = [];
+	this.private = args.pvt || args.private || false;
+	this.generateChart = args.chart || args.graph || false;
+	this.reactionVoting = args.reactions || args.rxn || false;
+	this.mult = this.reactionVoting || args.mult || args.multiple || false;
+	this.role = args.role || false;
+	this.footNote = opt.notes.trim() || '';
+
+	this.open = false;
+	this.totalVotes = 0;
+
+	// Initiate timer
+	this.startTimer = function() {
+		this.open = true;
+		setTimeout(function() {
+			this.open = false;
+		}.bind(this), opt.timeout * 60 * 1000);
+	};
+
+	// Log votes (if the poll is open and unlocked/user hasn't voted)
+	this.vote = function(emoji, user) {
+		if(this.open) {
+			if(this.lock && this.votedUsers.findIndex(el => el === user) !== -1) {
+				return "Sorry, this is a locked poll (you can't edit your vote) and you've already voted.";
+			} else {
+				this.choices[emoji].votes++;
+				this.votedUsers.push(user);
+				return `Great, I logged your vote for ${this.choices[emoji].name}!`;
+			}
+		} else {
+			return "Sorry, this poll has timed out and can no longer be voted on.";
+		}
+	};
+}
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -24,11 +89,13 @@ client.on('message', message => {
 			var choices = args.shift().slice(1, -1).split(',').map(Function.prototype.call, String.prototype.trim);
 			var options = {
 				title: title,
-				choices: choices
+				choices: choices,
+				emojiType: 'letters',
+				timeout: defaults.timeout,
+				arguments: {},
+				role: false,
+				notes: ''
 			};
-
-			// footNote will be used to store any error messsages/supplementary information
-			var footNote;
 
 			// args should now just have the arguments
 			args.forEach((arg, index) => {
@@ -39,36 +106,59 @@ client.on('message', message => {
 					// Remove '--'
 					arg = arg.slice(2);
 
-					if(arg === 'time') {
+					if(arg === 'time' || arg === 'timeout') {
 						let nextEl = args[index + 1];
 						// If the next element is a nunber
 						if(!isNaN(nextEl)) {
 							options.timeout = +nextEl;
+							args.slice(index + 1, 1);
 						} else {
-							let errorMessage = `A "time" argument was found, but the next item was not a number, so the poll defaulted to ${defaults.timeout} minutes. `;
+							let errorMessage = `A timeout argument was found, but the next item was not a number, so the poll defaulted to ${defaults.timeout} minutes. `;
 							console.warn(errorMessage);
-							footNote += errorMessage;
-							options.timeout = defaults.timeout;
+							options.notes += errorMessage;
 						}
 
 					} else if(arg === 'role') {
 						let nextEl = args[index + 1];
 						// If the next element is surrounded by double quotes
-						if(nextEl.charAt(0) === '"' && nextEl.charAt(nextEl.length - 1) === '"') {
+						if(args.find(el => el == 'rxn' || el === 'reactions')) {
+							let errorMessage = `A "role" argument was found, but the reactions option was enabled, so voting can't be restricted to roles.`;
+							console.warn(errorMessage);
+							footNote += errorMessage;
+						} else if(nextEl.charAt(0) === '"' && nextEl.charAt(nextEl.length - 1) === '"') {
 							options.role = nextEl.slice(1, -1);
+							args.slice(index + 1, 1);
 						} else {
 							let errorMessage = `A "role" argument was found, but the next item was not a string surrounded by "double quotes", so this was ignored. `;
 							console.warn(errorMessage);
-							footNote += errorMessage;
+							options.notes += errorMessage;
+						}
+
+					} else if(arg === 'numbers' || arg === 'num') {
+						if(choices.length <= emoji.numbers.length) {
+							options.emojiType = 'numbers';
+						} else {
+							let errorMessage = `The poll was requested to be displayed with number icons, but there are only ten icons and ${choices.length} options were specified, so this was ignored. `;
+							console.warn(errorMessage);
+							options.notes += errorMessage;
+						}
+
+					} else if(arg === 'yesno' || arg === 'yn') {
+						if(choices.length <= emoji.yn.length) {
+							options.emojiType = 'yn';
+						} else {
+							let errorMessage = `The poll was requested to be displayed with yes/no icons, but too many (${choices.length}) options were specified, so this was ignored. `;
+							console.warn(errorMessage);
+							options.notes += errorMessage;
 						}
 
 					} else {
-						options[arg] = true;
+						options.arguments[arg] = true;
 					}
 				}
 			});
 
-			console.log(options);
+			console.log(new Poll(options));
 
 		} else {
 			message.channel.send("Sorry, there was a problem with your messsage format. I was unable to make a poll.");
