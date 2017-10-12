c36d7cafdaaaae3cbc4817fe4b99f742759a7325
Ian Sanders <iansan5653@gmail.com>
2017-10-12 01:56:45 -0400

    Added embed generation function. Basic level functionality is mostly complete.

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const client = new Discord.Client();
 const defaults = {
 	timeout: 30,
 	color: 2555834,
-	triggers: {newPoll: '!newpoll', vote: '!vote'}
+	triggers: {newPoll: '!newpoll', vote: '!vote'},
+	appName: 'Votemaster'
 };
 var pollIndex = 0, polls = new Map();
 
@@ -14,7 +15,7 @@ const emoji = {
 	numbers: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'],
 	letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 		.map(value => 'regional_indicator_' + value),
-	yn: ['yes','no'],
+	yn: ['white_check_mark','negative_squared_cross_mark'],
 	maybe: 'shrug'
 };
 
@@ -48,9 +49,10 @@ class Poll {
 		this.restrictRole = args.role || false;
 		this.dontCloseEarly = args.lo || args.leaveopen || args.dontcloseearly || false;
 		this.timeout = opt.timeout || 30;
+		this.color = opt.color;
 
 		this.footNote = opt.notes || ' ';
-		this.footNote += `This is Poll \`${this.id}\`.`;
+		this.footNote += `This is Poll ${this.id}.`;
 
 		this.open = false;
 		this.totalVotes = 0;
@@ -58,6 +60,8 @@ class Poll {
 		this.voters = new Map();
 
 		this.server = opt.server;
+
+		this.timeCreated = new Date();
 	}
 
 	// Function to initiate timer
@@ -122,153 +126,182 @@ class Poll {
 	}
 }
 
+function generateDiscordEmbed(poll) {
+	var choiceList = ``;
+	poll.choices.forEach((choice, emoji) => {
+		choiceList += `:${emoji}: - ${choice.name} \n`;
+	});
+	var embed = {
+		title: `Poll ${poll.id}: ${poll.name}`,
+		description: `To vote, reply with\`!vote :emoji:\`. For example, "!vote ${poll.choices.entries().next()[0]}". If multiple polls are open, you\'ll have to specify which one using its ID number: \`!vote ${poll.id} :emoji:\`.`,
+		color: poll.color,
+		timestamp: poll.timeCreated,
+		footer: {
+			text: poll.footNote
+		},
+		author: {
+			name: defaults.appName
+		},
+		fields: [{
+			name: `Choices:`,
+			value: choiceList
+		}]
+	};
+
+	return embed;
+}
+
 client.on('ready', () => {
 	console.log('I am ready!');
 });
 
 client.on('message', message => {
-	// Array with: anything in brackets, anything in quotes, anything separated by spaces (in that hierarchy)
-	var args = message.content.trim().match(/(?:[^\s"\[]+|\[[^\[]*\]|"[^"]*")+/g);
+	if(message.content) {
+		// Array with: anything in brackets, anything in quotes, anything separated by spaces (in that hierarchy)
+		var args = message.content.trim().match(/(?:[^\s"\[]+|\[[^\[]*\]|"[^"]*")+/g);
+		if(args[0].toLowerCase() === defaults.triggers.newPoll) {
+			args.shift();
+			// Do a little format checking to make sure (first argument, title, should be in quotes, and second argument, choices, should be in brackets)
+			if(
+				args.length > 1 &&
+				args[0].charAt(0) === '"' &&
+				args[0].charAt(args[0].length - 1) === '"' &&
+				args[1].charAt(0) === '[' &&
+				args[1].charAt(args[1].length - 1) === ']'
+			) {
+				
+				// Title of the poll, without quotes
+				var title = args.shift().slice(1,-1);
+				// Array of poll choices, trimmed
+				var choices = args.shift().slice(1,-1).split(',').map(Function.prototype.call, String.prototype.trim);
+				var options = {
+					name: title,
+					choices: choices,
+					emojiType: 'letters',
+					timeout: defaults.timeout,
+					color: defaults.color,
+					arguments: {},
+					role: false,
+					notes: '',
+					server: message.guild
+				};
 
-	if(args[0].toLowerCase() === defaults.triggers.newPoll) {
-		args.shift();
-		// Do a little format checking to make sure (first argument, title, should be in quotes, and second argument, choices, should be in brackets)
-		if(
-			args.length > 1 &&
-			args[0].charAt(0) === '"' &&
-			args[0].charAt(args[0].length - 1) === '"' &&
-			args[1].charAt(0) === '[' &&
-			args[1].charAt(args[1].length - 1) === ']'
-		) {
-			
-			// Title of the poll, without quotes
-			var title = args.shift().slice(1,-1);
-			// Array of poll choices, trimmed
-			var choices = args.shift().slice(1,-1).split(',').map(Function.prototype.call, String.prototype.trim);
-			var options = {
-				name: title,
-				choices: choices,
-				emojiType: 'letters',
-				timeout: defaults.timeout,
-				color: defaults.color,
-				arguments: {},
-				role: false,
-				notes: '',
-				server: message.guild
-			};
+				// args should now just have the arguments
+				args.forEach((arg, index) => {
+					// If it's a new argument (starts with '--')
+					if(arg.charAt(0) === '-' && arg.charAt(1) === '-') {
 
-			// args should now just have the arguments
-			args.forEach((arg, index) => {
-				// If it's a new argument (starts with '--')
-				if(arg.charAt(0) === '-' && arg.charAt(1) === '-') {
+						// Remove '--'
+						arg = arg.slice(2);
 
-					// Remove '--'
-					arg = arg.slice(2);
+						if(arg === 'time' || arg === 'timeout') {
+							let nextEl = args[index + 1];
+							// If the next element is a nunber
+							if(!isNaN(nextEl) && nextEl > 0) {
+								options.timeout = +nextEl;
+								args.slice(index + 1, 1);
+							} else {
+								let errorMessage = `A timeout argument was found, but the next item was not a valid number, so the poll defaulted to ${defaults.timeout} minutes. `;
+								console.warn(errorMessage);
+								options.notes += errorMessage;
+							}
 
-					if(arg === 'time' || arg === 'timeout') {
-						let nextEl = args[index + 1];
-						// If the next element is a nunber
-						if(!isNaN(nextEl) && nextEl > 0) {
-							options.timeout = +nextEl;
-							args.slice(index + 1, 1);
+						} else if(arg === 'color' || arg === 'colour') {
+							let nextEl = args[index + 1];
+							// If the next element is a valid RGB int code
+							if(!isNaN(nextEl) && +nextEl >= 0 && +nextEl <= 256*256*256) {
+								options.color = +nextEl;
+								console.log(options.color);
+								args.slice(index + 1, 1);
+							} else {
+								let errorMessage = `A color argument was found, but the next item was not an RGB int code, so this was ignored.`;
+								console.warn(errorMessage);
+								options.notes += errorMessage;
+							}
+
+						} else if(arg === 'role') {
+							let nextEl = args[index + 1];
+							// If the next element is surrounded by double quotes
+							if(args.find(el => el == 'rxn' || el === 'reactions')) {
+								let errorMessage = `A "role" argument was found, but the reactions option was enabled, so voting can't be restricted to roles.`;
+								console.warn(errorMessage);
+								footNote += errorMessage;
+							} else if(nextEl.charAt(0) === '"' && nextEl.charAt(nextEl.length - 1) === '"') {
+								options.role = nextEl.slice(1, -1);
+								args.slice(index + 1, 1);
+							} else {
+								let errorMessage = `A "role" argument was found, but the next item was not a string surrounded by "double quotes", so this was ignored. `;
+								console.warn(errorMessage);
+								options.notes += errorMessage;
+							}
+
+						} else if(arg === 'numbers' || arg === 'num') {
+							if(choices.length <= emoji.numbers.length) {
+								options.emojiType = 'numbers';
+							} else {
+								let errorMessage = `The poll was requested to be displayed with number icons, but there are only ten icons and ${choices.length} options were specified, so this was ignored. `;
+								console.warn(errorMessage);
+								options.notes += errorMessage;
+							}
+
+						} else if(arg === 'yesno' || arg === 'yn') {
+							if(choices.length <= emoji.yn.length) {
+								options.emojiType = 'yn';
+							} else {
+								let errorMessage = `The poll was requested to be displayed with yes/no icons, but too many (${choices.length}) options were specified, so this was ignored. `;
+								console.warn(errorMessage);
+								options.notes += errorMessage;
+							}
+
 						} else {
-							let errorMessage = `A timeout argument was found, but the next item was not a valid number, so the poll defaulted to ${defaults.timeout} minutes. `;
-							console.warn(errorMessage);
-							options.notes += errorMessage;
+							options.arguments[arg] = true;
 						}
-
-					} else if(arg === 'color' || arg === 'colour') {
-						let nextEl = args[index + 1];
-						// If the next element is a valid RGB int code
-						if(!isNaN(nextEl) && +nextEl >= 0 && +nextEl <= 256*256*256) {
-							options.color = +nextEl;
-							args.slice(index + 1, 1);
-						} else {
-							let errorMessage = `A color argument was found, but the next item was not an RGB int code, so this was ignored.`;
-							console.warn(errorMessage);
-							options.notes += errorMessage;
-						}
-
-					} else if(arg === 'role') {
-						let nextEl = args[index + 1];
-						// If the next element is surrounded by double quotes
-						if(args.find(el => el == 'rxn' || el === 'reactions')) {
-							let errorMessage = `A "role" argument was found, but the reactions option was enabled, so voting can't be restricted to roles.`;
-							console.warn(errorMessage);
-							footNote += errorMessage;
-						} else if(nextEl.charAt(0) === '"' && nextEl.charAt(nextEl.length - 1) === '"') {
-							options.role = nextEl.slice(1, -1);
-							args.slice(index + 1, 1);
-						} else {
-							let errorMessage = `A "role" argument was found, but the next item was not a string surrounded by "double quotes", so this was ignored. `;
-							console.warn(errorMessage);
-							options.notes += errorMessage;
-						}
-
-					} else if(arg === 'numbers' || arg === 'num') {
-						if(choices.length <= emoji.numbers.length) {
-							options.emojiType = 'numbers';
-						} else {
-							let errorMessage = `The poll was requested to be displayed with number icons, but there are only ten icons and ${choices.length} options were specified, so this was ignored. `;
-							console.warn(errorMessage);
-							options.notes += errorMessage;
-						}
-
-					} else if(arg === 'yesno' || arg === 'yn') {
-						if(choices.length <= emoji.yn.length) {
-							options.emojiType = 'yn';
-						} else {
-							let errorMessage = `The poll was requested to be displayed with yes/no icons, but too many (${choices.length}) options were specified, so this was ignored. `;
-							console.warn(errorMessage);
-							options.notes += errorMessage;
-						}
-
-					} else {
-						options.arguments[arg] = true;
 					}
+				});
+
+				var newPoll = new Poll(options);
+				newPoll.startTimer();
+				polls.set(newPoll.id, newPoll);
+
+				var embed = generateDiscordEmbed(newPoll);
+				message.channel.send("OK, here's your poll:", {embed});
+
+			} else {
+				console.error("Message format was invalid.");
+				message.channel.send(`Poll requests must at minimum include a title (in "double quotes") and a set of options (in [square brackets], separated by commas). For example, try \`${defaults.trigger} "What is your favorite shade of red?" [dark red, medium red, light red]\`.`);
+			}
+
+		} else if(args[0].toLowerCase() == defaults.triggers.vote) {
+			args.shift();
+
+			var activePollsInServer = [], voteResponse;
+			polls.forEach(poll => {
+				if(poll.open && poll.server == message.guild) {
+					activePollsInServer.push(poll.id);
 				}
 			});
 
-			var newPoll = new Poll(options);
-			newPoll.startTimer();
-			polls.set(newPoll.id, newPoll);
-			console.log(polls);
+			if(activePollsInServer.length === 0) {
+				voteResponse = `There aren't any active polls in this server right now, so you can't vote.`;
 
-		} else {
-			console.error("Message format was invalid.");
-			message.channel.send(`Poll requests must at minimum include a title (in "double quotes") and a set of options (in [square brackets], separated by commas). For example, try \`${defaults.trigger} "What is your favorite shade of red?" [dark red, medium red, light red]\`.`);
-		}
-
-	} else if(args[0].toLowerCase() == defaults.triggers.vote) {
-		args.shift(0);
-
-		var activePollsInServer = [], voteResponse;
-		polls.forEach(poll => {
-			if(poll.open && poll.server == message.guild) {
-				activePollsInServer.push(poll.id);
-			}
-		});
-
-		if(activePollsInServer.length === 0) {
-			voteResponse = `There aren't any active polls in this server right now, so you can't vote.`;
-
-		} else if(isNaN(args[0].trim())) {
-			// Only the vote emoji was supplied
-			if(activePollsInServer.length === 1) {
-				voteResponse = polls.get(activePollsInServer[0]).vote(args[0], message.author).message;
+			} else if(isNaN(args[0].trim())) {
+				// Only the vote emoji was supplied
+				if(activePollsInServer.length === 1) {
+					voteResponse = polls.get(activePollsInServer[0]).vote(args[0], message.author).message;
+				} else {
+					voteResponse = 'Sorry, I don\'t know which poll to vote on. Please specify the poll id number before your vote.';
+				}
 			} else {
-				voteResponse = 'Sorry, I don\'t know which poll to vote on. Please specify the poll id number before your vote.';
-			}
-		} else {
-			// The ID and emoji were supplied
-			if(polls.get(args[0])) {
-				voteResponse = polls.get(args[0]).vote(args[1], message.author).message;
-			} else {
-				voteResponse = 'Sorry, that\'s not a valid poll to vote on. Please specify the poll id number before your vote.';
-			}
- 		}
+				// The ID and emoji were supplied
+				if(polls.get(args[0])) {
+					voteResponse = polls.get(args[0]).vote(args[1], message.author).message;
+				} else {
+					voteResponse = 'Sorry, that\'s not a valid poll to vote on. Please specify the poll id number before your vote.';
+				}
+	 		}
 
- 		message.channel.send(voteResponse);
+	 		message.channel.send(voteResponse);
+	 	}
 	}
 });
 
