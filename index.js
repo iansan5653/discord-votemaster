18f029a10c1712f5ad1dae08a223563a14c61bbd
Ian Sanders <isanders@chromalloy.com>
2019-03-14 09:32:36 -0400

    Fix remaining formatting problems

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const defaults = {
     appName: 'Votemaster'
 };
 let pollIndex = 0;
-let polls = new Map();
+const polls = new Map();
 
 /**
  * The corresponding emojis are used as unique keys for choices within each
@@ -39,7 +39,7 @@ class Poll {
      * @param {Object} opt
      */
     constructor(opt) {
-        let args = opt.arguments;
+        const args = opt.arguments;
         this.name = opt.name;
         this.id = pollIndex;
         pollIndex++;
@@ -90,7 +90,7 @@ class Poll {
         }.bind(this), this.timeout * 60 * 1000);
     }
 
-    /** 
+    /**
      * Log votes (if the poll is open and unlocked/user hasn't voted)
      * @param {string} key
      * @param {string} user
@@ -113,7 +113,7 @@ class Poll {
                 };
             } else if (this.voters.get(user.id)) {
                 // User has already voted, we need to change their vote
-                let oldVoter = this.voters.get(user.id);
+                const oldVoter = this.voters.get(user.id);
                 this.choices.get(oldVoter.vote.choice).votes--;
 
                 this.choices.get(key).votes++;
@@ -176,81 +176,89 @@ class Poll {
     }
 }
 
+/**
+ * Generate the Discord fancy embed object.
+ * @param {Poll} poll Poll object.
+ * @param {Symbol} type Type of embed.
+ * @return {Object}
+ */
 function generateDiscordEmbed(poll, type) {
-    var embed = {}, choiceList = ``, resultsList = ``;
+    let embed = {};
+    let choiceList = ``;
+    let resultsList = ``;
     poll.choices.forEach((choice, key) => {
         choiceList += `${choice.emoji} - ${choice.name} \n`;
         resultsList += `***${choice.votes} votes*** \n`;
     });
 
     switch (type) {
-        case EMBED_TYPES.POLL:
-            embed = {
-                title: `Poll ${poll.id}: ${poll.name}`,
-                description: `To vote, reply with\`!vote choice\` within the next ${poll.timeout} minutes. For example, "!vote ${poll.choices.keys().next().value}". If multiple polls are open, you\'ll have to specify which one using its number and a pound sign: \`!vote #${poll.id} choice\`.`,
-                color: poll.color,
-                timestamp: poll.timeCreated,
-                footer: {
-                    text: poll.footNote
-                },
-                author: {
-                    name: defaults.appName
-                },
-                fields: [{
-                    name: `Choices:`,
-                    value: choiceList
-                }]
-            };
-            break;
-        case EMBED_TYPES.RESULTS:
-            //TODO: Order choices in results based on number of votes
+    case EMBED_TYPES.POLL:
+        embed = {
+            title: `Poll ${poll.id}: ${poll.name}`,
+            description: `To vote, reply with\`!vote choice\` within the next ${poll.timeout} minutes. For example, "!vote ${poll.choices.keys().next().value}". If multiple polls are open, you\'ll have to specify which one using its number and a pound sign: \`!vote #${poll.id} choice\`.`,
+            color: poll.color,
+            timestamp: poll.timeCreated,
+            footer: {
+                text: poll.footNote
+            },
+            author: {
+                name: defaults.appName
+            },
+            fields: [{
+                name: `Choices:`,
+                value: choiceList
+            }]
+        };
+        break;
+    case EMBED_TYPES.RESULTS:
+        // TODO: Order choices in results based on number of votes
 
-            embed = {
-                title: `*Results* - Poll ${poll.id}: ${poll.name}`,
-                description: poll.open ? `This poll is still open, so these results may change.` : `This poll has closed and cannot be voted on.`,
-                color: poll.color,
-                timestamp: new Date(),
-                footer: {
-                    text: `For more detailed results, use the \`--users\` flag.`
-                },
-                author: {
-                    name: defaults.appName
-                },
-                fields: [{
-                    name: `Choices:`,
-                    value: choiceList,
-                    inline: true
-                }, {
-                    name: `Results:`,
-                    value: resultsList,
-                    inline: true
-                }]
-            };
-            break;
-        case EMBED_TYPES.DETAIL_RESULTS:
-            //TODO: Order choices in results based on number of votes
+        embed = {
+            title: `*Results* - Poll ${poll.id}: ${poll.name}`,
+            description: poll.open ? `This poll is still open, so these results may change.` : `This poll has closed and cannot be voted on.`,
+            color: poll.color,
+            timestamp: new Date(),
+            footer: {
+                text: `For more detailed results, use the \`--users\` flag.`
+            },
+            author: {
+                name: defaults.appName
+            },
+            fields: [{
+                name: `Choices:`,
+                value: choiceList,
+                inline: true
+            }, {
+                name: `Results:`,
+                value: resultsList,
+                inline: true
+            }]
+        };
+        break;
+    case EMBED_TYPES.DETAIL_RESULTS:
+        // TODO: Order choices in results based on number of votes
 
-            embed = {
-                title: `*Results* - Poll ${poll.id}: ${poll.name}`,
-                description: poll.open ? `This poll is still open, so these results may change.` : `This poll has closed and cannot be voted on.`,
-                color: poll.color,
-                timestamp: new Date(),
-                footer: {
-                    text: `We don't have detailed results capability yet.`
-                },
-                author: {
-                    name: defaults.appName
-                },
-                fields: [{
-                    name: `Choices:`,
-                    value: choiceList,
-                    inline: true
-                }, {
-                    name: `Results:`,
-                    value: resultsList,
-                    inline: true
-                }]
-            };
+        embed = {
+            title: `*Results* - Poll ${poll.id}: ${poll.name}`,
+            description: poll.open ? `This poll is still open, so these results may change.` : `This poll has closed and cannot be voted on.`,
+            color: poll.color,
+            timestamp: new Date(),
+            footer: {
+                text: `We don't have detailed results capability yet.`
+            },
+            author: {
+                name: defaults.appName
+            },
+            fields: [{
+                name: `Choices:`,
+                value: choiceList,
+                inline: true
+            }, {
+                name: `Results:`,
+                value: resultsList,
+                inline: true
+            }]
+        };
     }
 
     return embed;
@@ -262,8 +270,8 @@ client.on('ready', () => {
 
 client.on('message', discordMessage => {
     if (discordMessage.content) {
-        let message = messageProcessing.Message(discordMessage.content);
-        let args = message.args;
+        const message = new messageProcessing.Message(discordMessage.content);
+        const args = message.args;
         if (message.command === defaults.triggers.newPoll) {
             // Do a little format checking to make sure (first argument, title, should be in quotes, and second argument, choices, should be in brackets)
             if (
@@ -273,12 +281,11 @@ client.on('message', discordMessage => {
                 args[1].charAt(0) === '[' &&
                 args[1].charAt(args[1].length - 1) === ']'
             ) {
-
                 // Title of the poll, without quotes
-                var title = args.shift().slice(1, -1);
+                const title = args.shift().slice(1, -1);
                 // Array of poll choices, trimmed
-                var choices = args.shift().slice(1, -1).split(',').map(Function.prototype.call, String.prototype.trim);
-                var options = {
+                const choices = args.shift().slice(1, -1).split(',').map(Function.prototype.call, String.prototype.trim);
+                const options = {
                     name: title,
                     choices: choices,
                     emojiType: 'letters',
@@ -294,88 +301,81 @@ client.on('message', discordMessage => {
                 args.forEach((arg, index) => {
                     // If it's a new argument (starts with '--')
                     if (arg.charAt(0) === '-' && arg.charAt(1) === '-') {
-
                         // Remove '--'
                         arg = arg.slice(2);
 
                         if (arg === 'time' || arg === 'timeout') {
-                            let nextEl = args[index + 1];
+                            const nextEl = args[index + 1];
                             // If the next element is a nunber
                             if (!isNaN(nextEl) && nextEl > 0) {
                                 options.timeout = +nextEl;
                                 args.slice(index + 1, 1);
                             } else {
-                                let errorMessage = `A timeout argument was found, but the next item was not a valid number, so the poll defaulted to ${defaults.timeout} minutes. `;
+                                const errorMessage = `A timeout argument was found, but the next item was not a valid number, so the poll defaulted to ${defaults.timeout} minutes. `;
                                 console.warn(errorMessage);
                                 options.notes += errorMessage;
                             }
-
                         } else if (arg === 'color' || arg === 'colour') {
-                            let nextEl = args[index + 1];
+                            const nextEl = args[index + 1];
                             // If the next element is a valid RGB int code
                             if (!isNaN(nextEl) && +nextEl >= 0 && +nextEl <= 256 * 256 * 256) {
                                 options.color = +nextEl;
                                 args.slice(index + 1, 1);
                             } else {
-                                let errorMessage = `A color argument was found, but the next item was not an RGB int code, so this was ignored.`;
+                                const errorMessage = `A color argument was found, but the next item was not an RGB int code, so this was ignored.`;
                                 console.warn(errorMessage);
                                 options.notes += errorMessage;
                             }
-
                         } else if (arg === 'role') {
-                            let nextEl = args[index + 1];
+                            const nextEl = args[index + 1];
                             // If the next element is surrounded by double quotes
                             if (args.find(el => el == 'rxn' || el === 'reactions')) {
-                                let errorMessage = `A "role" argument was found, but the reactions option was enabled, so voting can't be restricted to roles.`;
+                                const errorMessage = `A "role" argument was found, but the reactions option was enabled, so voting can't be restricted to roles.`;
                                 console.warn(errorMessage);
                                 footNote += errorMessage;
                             } else if (nextEl.charAt(0) === '"' && nextEl.charAt(nextEl.length - 1) === '"') {
                                 options.role = nextEl.slice(1, -1);
                                 args.slice(index + 1, 1);
                             } else {
-                                let errorMessage = `A "role" argument was found, but the next item was not a string surrounded by "double quotes", so this was ignored. `;
+                                const errorMessage = `A "role" argument was found, but the next item was not a string surrounded by "double quotes", so this was ignored. `;
                                 console.warn(errorMessage);
                                 options.notes += errorMessage;
                             }
-
                         } else if (arg === 'numbers' || arg === 'num') {
                             if (choices.length <= emoji.numbers.length) {
                                 options.emojiType = 'numbers';
                             } else {
-                                let errorMessage = `The poll was requested to be displayed with number icons, but there are only ten icons and ${choices.length} options were specified, so this was ignored. `;
+                                const errorMessage = `The poll was requested to be displayed with number icons, but there are only ten icons and ${choices.length} options were specified, so this was ignored. `;
                                 console.warn(errorMessage);
                                 options.notes += errorMessage;
                             }
-
                         } else if (arg === 'yesno' || arg === 'yn') {
                             if (choices.length <= emoji.yn.length) {
                                 options.emojiType = 'yn';
                             } else {
-                                let errorMessage = `The poll was requested to be displayed with yes/no icons, but too many (${choices.length}) options were specified, so this was ignored. `;
+                                const errorMessage = `The poll was requested to be displayed with yes/no icons, but too many (${choices.length}) options were specified, so this was ignored. `;
                                 console.warn(errorMessage);
                                 options.notes += errorMessage;
                             }
-
                         } else {
                             options.arguments[arg] = true;
                         }
                     }
                 });
 
-                var newPoll = new Poll(options);
+                const newPoll = new Poll(options);
                 newPoll.startTimer();
                 polls.set(newPoll.id, newPoll);
 
-                let embed = generateDiscordEmbed(newPoll, 'poll');
+                const embed = generateDiscordEmbed(newPoll, 'poll');
                 discordMessage.channel.send('OK, here\'s your poll:', {embed});
-
             } else {
                 console.error('Message format was invalid.');
                 discordMessage.channel.send(`Poll requests must at minimum include a title (in "double quotes") and a set of options (in [square brackets], separated by commas). For example, try \`${defaults.triggers.newPoll} "What is your favorite shade of red?" [dark red, medium red, light red]\`.`);
             }
-
         } else if (message.command == defaults.triggers.vote) {
-            var activePollsInServer = [], voteResponse;
+            const activePollsInServer = [];
+            let voteResponse;
             polls.forEach(poll => {
                 if (poll.open && poll.server == discordMessage.guild) {
                     activePollsInServer.push(poll.id);
@@ -384,7 +384,6 @@ client.on('message', discordMessage => {
 
             if (activePollsInServer.length === 0) {
                 voteResponse = `There aren't any active polls in this server right now, so you can't vote.`;
-
             } else if (args[0].charAt(0) !== '#') {
                 // Only the vote was supplied
                 if (activePollsInServer.length === 1) {
@@ -393,10 +392,9 @@ client.on('message', discordMessage => {
                     // TODO dynamic examples
                     voteResponse = 'Sorry, I don\'t know which poll to vote on. Please specify the poll id number using a pound sign and a number (ie \'!vote #1 A\') before your vote.';
                 }
-
             } else {
                 // The ID and vote were supplied
-                let pollID = +(args[0].substr(1));
+                const pollID = +(args[0].substr(1));
 
                 if (activePollsInServer.includes(pollID)) {
                     voteResponse = polls.get(pollID).vote(args[1].toLowerCase(), discordMessage.author).message;
@@ -407,14 +405,11 @@ client.on('message', discordMessage => {
             }
 
             discordMessage.channel.send(voteResponse);
-
         } else if (message.command == defaults.triggers.results) {
-            var response;
-
             if (args[0].charAt(0) !== '#') {
                 discordMessage.channel.send('Sorry, I don\'t know which poll to get results for. Please specify the poll id number using a pound sign and number (ie \'!results #1\').');
             } else {
-                let pollID = +(args[0].substr(1));
+                const pollID = +(args[0].substr(1));
 
                 if (polls.get(pollID)) {
                     let embed;
@@ -429,9 +424,8 @@ client.on('message', discordMessage => {
                     discordMessage.channel.send('Sorry, that poll doesn\'t seem to exist.');
                 }
             }
-
         } else if (message.command == '!pollping') {
-            discordMessage.channel.send('PONG!'); //for testing connection
+            discordMessage.channel.send('PONG!'); // for testing connection
         }
     }
 });
--- msg/processing.js
+++ msg/processing.js
@@ -4,22 +4,22 @@
 
 /** Class representing a message, with information about message contents. */
 class Message {
-    /** 
+    /**
      * Construct a new message and perform initial parsing.
      * @param {string} rawMessage The original message string.
     */
     constructor(rawMessage) {
         /** The original message string. */
-        this.rawMessage = rawMessage
-        let messageComponents = Message.breakMessage(this.rawMessage)
+        this.rawMessage = rawMessage;
+        let messageComponents = Message.breakMessage(this.rawMessage);
 
         /** The command associated with the message (first word in lowercase).*/
-        this.command = messageComponents[0].toLowerCase()
-        /** 
+        this.command = messageComponents[0].toLowerCase();
+        /**
          * The rest of the message components as defined by
          * {@link Message.breakMessage}.
          */
-        this.args = messageComponents.shift()
+        this.args = messageComponents.shift();
     }
 
     /**
@@ -27,9 +27,11 @@ class Message {
      * Returns a parsed array with: anything in brackets, anything in quotes,
      * anything separated by spaces (in that hierarchy) 
      * @param {string} rawMessage The original message string.
-     * @returns {[string]}
+     * @return {[string]}
      */
     static breakMessage(rawMessage) {
         return this.rawMessage.content.trim().match(/(?:[^\s"\[]+|\[[^\[]*\]|"[^"]*")+/g);
     }
 }
+
+exports.Message = Message;
